a83b04ab5624f627bdf5dc856fad13deba609c85
Dinma-P <phaytep@gmail.com>
2023-03-06 17:45:38 +0100

    Create Routes for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 // import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Containers/Header";
+import ProductDetails from "./Containers/ProductDetails";
+import ProductListing from "./Containers/ProductListing";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <Fragment>
+          <Header />
+          <Routes>
+            <Route path="/" exact component={ProductListing} />
+            <Route
+              path="/product/:productid"
+              exact
+              component={ProductDetails}
+            />
+            <Route>404 Not Found!</Route>
+          </Routes>
+        </Fragment>
+      </Router>
     </div>
   );
 }
