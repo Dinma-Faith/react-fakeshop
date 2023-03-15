f30a7ce223821816e386e676883ca629d37ad3ff
Dinma-P <phaytep@gmail.com>
2023-03-15 15:56:37 +0100

    Replace fragment with Provider teg and replace component with element in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import './App.css';
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 import Header from "./Containers/Header";
 import ProductDetails from "./Containers/ProductDetails";
 import ProductListing from "./Containers/ProductListing";
@@ -8,20 +9,21 @@ import ProductListing from "./Containers/ProductListing";
 function App() {
   return (
     <div className="App">
+      <Provider store={store}>
       <Router>
-        <Fragment>
           <Header />
           <Routes>
-            <Route path="/" exact component={ProductListing} />
+            <Route path="/" exact element={<ProductListing/>} />
             <Route
               path="/product/:productid"
               exact
-              component={ProductDetails}
+              element={<ProductDetails/>}
             />
             <Route>404 Not Found!</Route>
           </Routes>
-        </Fragment>
       </Router>
+      </Provider>
+
     </div>
   );
 }
