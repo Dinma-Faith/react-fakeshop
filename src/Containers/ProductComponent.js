2068320b5d49a3ae9278635c9f0d9ccb1ffea9f7
Dinma-P <phaytep@gmail.com>
2023-03-16 18:56:33 +0100

    Add link tag to product component

--- src/Containers/ProductComponent.js
+++ src/Containers/ProductComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "../App.css";
 
@@ -8,15 +9,19 @@ const ProductComponent = () => {
     const { id, title, image, price, category } = product;
     return (
       <div key={id} className="product-components">
-        <div className="products-img-and-content">
-          <div className="img-div">
-            <img src={image} alt={title} />
-          </div>
+        <Link to={`/product/${id}`}>
+          <div className="products-img-and-content">
+            <div className="img-div">
+              <img src={image} alt={title} className="main-img" />
+            </div>
 
-          <h5>{title}</h5>
-          <h5>Amt: {price}</h5>
-          <small>Type: {category}</small>
-        </div>
+            <div className="product-content">
+              <h5>{title}</h5>
+              <p>Amt: {price}</p>
+              <small>Type: {category}</small>
+            </div>
+          </div>
+        </Link>
       </div>
     );
   });
